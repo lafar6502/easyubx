3a2f9f1a32db38e9c33e48b34897ecaa8a740715
Bernd Wiegmann <Bernd@bwiegmann.de>
2019-11-21 00:25:16 +0100

    Added license information and fürst constants definition

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Easy-UBX
 
-Easy to use library for the UBX protocol
+Easy-UBX is a C library to utilize the UBX binary protocol for the u-blox GPS receiver like NEO-6M and others. These GPS devices are often used on embedded IoT devices.  
+
+This library is a portable implementation in C and does not depend on any serial port implementation.
+
+Features currently under development:
+
+* Read GPS Version information
+* Read and change port settings
+* Configure protocol parameter for NMEA and UBX protocol
+* Change settins like navigation mode and update frequency
+
+Features which may be implemented at some later point:
+
+* Library implementation for Arduino
+* C++ Wrapper for Arduino and other platforms
+* command line tool for linux
+* reading position information to eliminate need for a NMEA library
---
+++ easyubx/src/easyubx.h
@@ -0,0 +1,103 @@
+/*
+ * Main include file for the Easy UBX library
+ */
+ 
+/*
+	 MIT License
+
+	Copyright (c) 2019 Bernd Wiegmann (bernd@iotinsights.de)
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+#ifndef EASYUBX_H
+#define EASYUBX_H
+
+#include <stdint.h>
+
+const uint8_t EUBX_SYNC1					= 0xb5;
+const uint8_t EUBX_SYNC2					= 0x62;
+
+const uint8_t EUBX_CLASS_NAV				= 0x01;
+const uint8_t EUBX_CLASS_RXM				= 0x02;
+const uint8_t EUBX_CLASS_INF				= 0x04;
+const uint8_t EUBX_CLASS_ACK				= 0x05;
+const uint8_t EUBX_CLASS_CFG				= 0x06;
+const uint8_t EUBX_CLASS_MON				= 0x0a;
+const uint8_t EUBX_CLASS_AID				= 0x0b;
+const uint8_t EUBX_CLASS_TIM				= 0x0d;
+const uint8_t EUBX_CLASS_LOG				= 0x21;
+
+const uint8_t EUBX_ID_ACK_ACK				= 0x01;
+const uint8_t EUBX_ID_ACK_NAK				= 0x00;
+
+const uint8_t EUBX_ID_AID_ALM				= 0x00;
+// TODO: to be continued
+
+const uint8_t EUBX_ID_CFG_ANT				= 0x13;
+const uint8_t EUBX_ID_CFG_CFG				= 0x09;
+const uint8_t EUBX_ID_CFG_DAT				= 0x06;
+const uint8_t EUBX_ID_CFG_GNSS				= 0x3d;
+const uint8_t EUBX_ID_CFG_INF				= 0x02;
+const uint8_t EUBX_ID_CFG_ITFM				= 0x39;
+const uint8_t EUBX_ID_CFG_LOGFILTER			= 0x47;
+const uint8_t EUBX_ID_CFG_MSG				= 0x01;
+const uint8_t EUBX_ID_CFG_NAV5				= 0x24;
+const uint8_t EUBX_ID_CFG_NAVX5				= 0x23;
+const uint8_t EUBX_ID_CFG_NMEA				= 0x17;
+const uint8_t EUBX_ID_CFG_PM2				= 0x3b;
+const uint8_t EUBX_ID_CFG_PRT				= 0x00;
+const uint8_t EUBX_ID_CFG_RATE				= 0x08;
+const uint8_t EUBX_ID_CFG_RINV				= 0x34;
+const uint8_t EUBX_ID_CFG_RST				= 0x04;
+const uint8_t EUBX_ID_CFG_RXM				= 0x11;
+const uint8_t EUBX_ID_CFG_SBAS				= 0x16;
+const uint8_t EUBX_ID_CFG_TP5				= 0x31;
+const uint8_t EUBX_ID_CFG_USB				= 0x1b;
+
+const uint8_t EUBX_ID_INF_DEBUG				= 0x04;
+const uint8_t EUBX_ID_INF_ERROR				= 0x00;
+const uint8_t EUBX_ID_INF_NOTICE			= 0x02;
+const uint8_t EUBX_ID_INF_TEST				= 0x03;
+const uint8_t EUBX_ID_INF_WARNING			= 0x01;
+
+const uint8_t EUBX_ID_LOG_CREATE			= 0x07;
+const uint8_t EUBX_ID_LOG_ERASE				= 0x03;
+const uint8_t EUBX_ID_LOG_FINDTIME			= 0x0e;
+const uint8_t EUBX_ID_LOG_INFO				= 0x08;
+const uint8_t EUBX_ID_LOG_RETRIEVEPOS		= 0x0b;
+const uint8_t EUBX_ID_LOG_RETRIEVESTRING	= 0x0d;
+const uint8_t EUBX_ID_LOG_RETRIEVE			= 0x09;
+const uint8_t EUBX_ID_LOG_STRING			= 0x04;
+
+typedef enum {
+	EUBX_ERROR_OK = 0,
+	EUBX_ERROR_NULLPTR = -1
+} TEasyUBXError;
+
+typedef struct {
+	bool 			is_initialized;			// is set to true if handle is initialized
+	TEasyUBXError	last_error;				// last error code, will be OK if an operation was successful
+} TEasyUBXHandle;
+
+TEasyUBXError eubx_init(TEasyUBXHandle * pHandle);
+
+TEasyUBXError eubx_receive_byte(TEasyUBXHandle * pHandle, uint8_t byte);
+
+#endif  /* EASYUBX_H */
